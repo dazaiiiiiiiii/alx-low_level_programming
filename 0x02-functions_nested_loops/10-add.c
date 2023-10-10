75397bb8bdd4708427039459e6231133d1fad3db
jehovah <abdessamadmazighi123@gmail.com>
2023-10-10 15:28:00 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function that adds two ints and returns the result
+ *
+ * @i: take the input
+ * @j: take the input
+ *
+ * Return: return the result
+ */
+
+int add(int i, int j)
+{
+	int k = 0;
+
+	return (k = i + j);
+}
