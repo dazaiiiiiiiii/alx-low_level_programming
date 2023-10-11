679b301a165ca0ad39f82b83f12a91b83881a508
jehovah <abdessamadmazighi123@gmail.com>
2023-10-10 18:11:12 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.c"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int num = 0;
+	int sum = 0;
+
+	while (num < 1024)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+		num++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
