43c4e9024b902de7a511564fe875af80344a2cc9
jehovah <abdessamadmazighi123@gmail.com>
2023-10-12 13:27:27 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 tmes the numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j <= 10)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i <= 9)
+
+				_putchar(i + '0');
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
