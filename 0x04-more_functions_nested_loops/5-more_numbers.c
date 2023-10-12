709f0ac62567620cc8fd43e75844675b7645ec5b
jehovah <abdessamadmazighi123@gmail.com>
2023-10-12 13:38:24 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,14 +14,9 @@ void more_numbers(void)
 		i = 0;
 		while (i <= 14)
 		{
-			if (i <= 9)
-
-				_putchar(i + '0');
-			else
-			{
+			if (i >= 10 && i <= 14)
 				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+			_putchar(i % 10 + '0');
 			i++;
 		}
 		_putchar('\n');
