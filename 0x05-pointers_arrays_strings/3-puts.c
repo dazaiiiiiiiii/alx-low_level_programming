902c2b9e5b09834b938cba810e05ffef92a1be7b
jehovah <abdessamadmazighi123@gmail.com>
2023-10-16 14:01:30 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts  - function that prints a string followed by a new line
+ * @str: take a string
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+}
