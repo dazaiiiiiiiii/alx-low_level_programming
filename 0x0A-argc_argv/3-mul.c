02e056c1d97f75a4f48c8308cab187b670768397
jehovah <abdessamadmazighi123@gmail.com>
2023-10-27 13:17:14 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument counter
+ * @av: argument value
+ * Return: Always 0 (Success) otherwise return 1
+ */
+
+int main(int ac, char **av)
+{
+	int multi;
+
+	if (ac == 3)
+	{
+		multi = atoi(av[1]) * atoi(av[2]);
+		printf("%d\n", multi);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
