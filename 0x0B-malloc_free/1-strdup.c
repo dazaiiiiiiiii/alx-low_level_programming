8cc82373fe8d35f9e5e764f5e07167b07fe31075
jehovah <abdessamadmazighi123@gmail.com>
2023-10-30 08:33:14 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that return *p copy of the str given as a param
+ *
+ * @str: input str
+ * Return: 0 if str is NULL otherwise return the array
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int size = 0;
+	int i;
+
+	while (str[size])
+		size++;
+	if (size == 0)
+		return (0);
+	s = malloc(size * sizeof(char) + 1);
+	if (!s)
+		return (0);
+	i = 0;
+	while (i < size)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
