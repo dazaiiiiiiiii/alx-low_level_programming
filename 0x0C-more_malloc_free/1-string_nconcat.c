18fec1f3937a147fd62f7834174550e95a9939ee
jehovah <abdessamadmazighi123@gmail.com>
2023-11-02 07:33:53 -0700

    strncat mallco

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - function that return the str's length
+ *
+ * @str: takes input
+ * Return: length
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
+ * string_nconcat - function that concatenates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes of s2
+ * Return: pointer if successe or 0 if fails
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *arr;
+	unsigned int i = 0;
+	int k;
+	unsigned int size;
+	int index;
+	unsigned int length;
+
+	if (*s1)
+		i = _strlen(s1);
+	size = i + n;
+	arr = malloc((size + 1) * sizeof(char));
+	if (!arr)
+		return (0);
+	k = 0;
+	while (k < i)
+	{
+		index = arr[k] = s1[k];
+		k++;
+	}
+	index = 0;
+	while (index < n)
+	{
+		arr[index + i] = s2[index];
+		index++;
+	}
+	length = k + index;
+	arr[length] = '\0';
+	return (arr);
+}
