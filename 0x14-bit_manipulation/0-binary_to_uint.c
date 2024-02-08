80522f49eeed0108d7d8cdffc3d64ac83514a3a4
root <abdessamadmazighi123@gmail.com>
2024-02-08 13:12:36 -0800

    convert bin to dic

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - function that converts a binary number to an uint
@@ -9,21 +10,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i, j, total, power;
+
 	if (b == NULL)
 		return (0);
-	int i;
-
 	for (i = 0; b[i]; i++)
 	{
 		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
 	}
-	int total;
-	int power;
-
 	total = 0;
 	power = 1;
-	for (int j = i - 1; j >= 0; j--)
+	for (j = i - 1; j >= 0; j--)
 	{
 		if (b[j] == '1')
 			total += power;
